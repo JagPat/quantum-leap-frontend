5f24f55b82b912c9edbcba58c3cf44ad5413d1f3
JagPat <jagrutpatel@gmail.com>
2025-07-19 14:39:58 +0530

    Fix main.jsx to use App component instead of Pages directly - resolves Router context issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Pages from '@/pages/index.jsx'
+import App from '@/App.jsx'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Pages />
+    <App />
 ) 
